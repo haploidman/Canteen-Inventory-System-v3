aa09f82f131a9c96a7d776ddeedd18a235e4a004
haploidman <cl.laudencia@gmail.com>
2026-05-10 22:29:44 +0800

    Delete CanteenDB directory

--- CanteenDB/Login.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE Login (
-        [Id] INT IDENTITY(1,1) PRIMARY KEY CLUSTERED ([Id] ASC),
-        [username] NVARCHAR(50) NOT NULL UNIQUE, -- Unique username constraint
-        [password] NVARCHAR(50) NOT NULL,
-        [role] NVARCHAR(50) NOT NULL CHECK (role IN ('user', 'admin')),
-        CreatedDate DATETIME DEFAULT GETDATE()
-    );
